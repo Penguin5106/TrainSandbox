6cb8a66cbff66ba3f00e078cc2457ba9248cfb5b
Penguin5106 <97694379+Penguin5106@users.noreply.github.com>
2025-10-26 01:33:49 +0100

    added additional functionality to linked graph based off UML diagram
    added functions:
    
    countNodes
    containsNodes
    addNode
    addRoot
    deleteNode
    
    I am going to have to write extensive unit tests for these

--- Assets/Scripts/Other/Data Structures/LinkedGraph.cs
+++ Assets/Scripts/Other/Data Structures/LinkedGraph.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using Unity.Hierarchy;
+using Unity.VisualScripting;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
@@ -15,10 +18,262 @@ public class LinkedGraph<T>
     public LinkedGraph(Node<T> root)
     {
         this.root = root;
-        count = 1;
+        countNodes();
+    }
+
+    public bool containsNode(Node<T> toFind)
+    {
+        int i = 0;
+        Node<T> tocheck = root;
+        
+        if (toFind == tocheck)
+            return true;
+
+        Dictionary<int, Node<T>> seen = new Dictionary<int, Node<T>>();
+        seen.Add(i, tocheck);
+        Dictionary<int, Node<T>> explored = new Dictionary<int, Node<T>>();
+
+        while (i < count)
+        {
+
+            foreach(Node<T> node in tocheck.links.Values)
+            {
+                if (tocheck.links.ContainsValue(toFind))
+                    return true;
+
+                if (!seen.ContainsValue(node))
+                    seen.Add(seen.Count, node);
+            }
+
+            explored.Add(explored.Count, tocheck);
+
+            foreach(Node<T> node in seen.Values)
+            {
+                if (!explored.ContainsValue(node))
+                {
+                    tocheck = node;
+                    continue;
+                }
+            }
+
+        }
+
+        return false;
+    }
+
+    public void countNodes()
+    {
+        Node<T> tocheck = root;
+
+        Dictionary<int, Node<T>> seen = new Dictionary<int, Node<T>>();
+        seen.Add(0, tocheck);
+        Dictionary<int, Node<T>> explored = new Dictionary<int, Node<T>>();
+
+        while (seen.Count != explored.Count )
+        {
+
+            foreach (Node<T> node in tocheck.links.Values)
+            {
+
+                if (!seen.ContainsValue(node))
+                    seen.Add(seen.Count, node);
+            }
+
+            explored.Add(explored.Count, tocheck);
+
+            foreach (Node<T> node in seen.Values)
+            {
+                if (!explored.ContainsValue(node))
+                {
+                    tocheck = node;
+                    continue;
+                }
+            }
+
+        }
+
+        count = seen.Count;
+
     }
     
-    
+    public bool addNode(Node<T> node, Node<T> link)
+    {
+        if (link == null || node == null || !containsNode(link) || containsNode(node)) 
+        {
+            return false;
+        }
+
+        node.links.Add(node.links.Count, link);
+        link.links.Add(link.links.Count, node);
+
+        count++;
+
+        return true;
+    }
+
+    public void addRoot(Node<T> node, Node<T> link)
+    {
+        if (containsNode(node))
+        {
+            root = node;
+
+            if (link == null || root.links.ContainsValue(link))
+                return;
+
+            root.links.Add(root.links.Count, link);
+            return;
+        }
+
+        if (addNode(node, link))
+        {
+            root = node;
+        }
+    }
+
+    public Dictionary<int, LinkedGraph<T>>  deleteNode(Node<T> node)
+    {
+        if (node == null || !containsNode(node)) return null;
+
+        if (node.links.Count == 0 && node == root)
+        { 
+            root = null;
+            count = 0;
+            return null;
+        }
+
+        if (node.links.Count == 1 && node == root)
+        {
+            foreach (Node<T> link in node.links.Values)
+            {
+                if (link != null)
+                { 
+                    root = link;
+                    count--;
+                    return null;
+                }
+            }
+        }
+
+        if (node.links.Count == 1)
+        {
+            foreach (Node<T> link in  node.links.Values)
+            {
+                foreach(KeyValuePair<int, Node<T>> linkPair in link.links)
+                {
+                    if (linkPair.Value == node)
+                    {
+                        link.links.Remove(linkPair.Key);
+                    }
+                }
+            }
+            count--;
+            return null;
+        }
+
+        Dictionary<int, LinkedGraph<T>> LooseGraphs = new Dictionary<int, LinkedGraph<T>>();
+
+        if (node.links.Count > 1 &&  node == root)
+        {
+            Dictionary<int, Node<T>> severedLinks = node.links;
+
+            foreach (Node<T> link in severedLinks.Values)
+            {
+                foreach (KeyValuePair<int, Node<T>> linkPair in link.links)
+                {
+                    if (linkPair.Value == node)
+                        link.links.Remove(linkPair.Key);
+                }
+            }
+
+            foreach(Node<T> link in severedLinks.Values)
+            {
+                LooseGraphs.Add(LooseGraphs.Count, new LinkedGraph<T>(link));
+            }
+
+            int i = 0;
+
+            while (i < LooseGraphs.Count-1)
+            {
+
+                int j = 1;
+                LinkedGraph<T> primaryGraph = null;
+                LooseGraphs.TryGetValue(i, out primaryGraph);
+
+                while (j < LooseGraphs.Count)
+                {
+                    LinkedGraph<T> comparison = null;
+                    LooseGraphs.TryGetValue(j, out comparison);
+
+                    if (primaryGraph.containsNode(comparison.root))
+                    {
+                        LooseGraphs.Remove(j);
+                    }
+
+                    j++;
+                }
+
+                root = primaryGraph.root;
+                i++;
+            }
+
+            return LooseGraphs;
+        }
+
+        if (node.links.Count > 1)
+        {
+            Dictionary<int, Node<T>> severedLinks = node.links;
+
+            foreach (Node<T> link in severedLinks.Values)
+            {
+                foreach (KeyValuePair<int, Node<T>> linkPair in link.links)
+                {
+                    if (linkPair.Value == node)
+                        link.links.Remove(linkPair.Key);
+                }
+            }
+
+            foreach (Node<T> link in severedLinks.Values)
+            {
+                LooseGraphs.Add(LooseGraphs.Count, new LinkedGraph<T>(link));
+            }
+
+            int i = 0;
+
+            while (i < LooseGraphs.Count - 1)
+            {
+
+                int j = 1;
+                LinkedGraph<T> primaryGraph = null;
+                LooseGraphs.TryGetValue(i, out primaryGraph);
+
+                if (primaryGraph.containsNode(root))
+                {
+                    LooseGraphs.Remove(i);
+                    i++;
+
+                    continue; 
+                }
+
+                while (j < LooseGraphs.Count)
+                {
+                    LinkedGraph<T> comparison = null;
+                    LooseGraphs.TryGetValue(j, out comparison);
+
+                    if (primaryGraph.containsNode(comparison.root))
+                    {
+                        LooseGraphs.Remove(j);
+                    }
+
+                    j++;
+                }
+                i++;
+            }
+
+            return LooseGraphs;
+        }
+
+        return null;
+    }
 }
 
 /*
